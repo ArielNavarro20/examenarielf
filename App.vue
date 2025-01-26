756172003bf6ab5323972dff8c80d6a9063b2bde
ArielNavarro20 <126171836+ArielNavarro20@users.noreply.github.com>
2025-01-26 11:34:00 -0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,7 @@
+<template>
+    <div id="nav">    
+        <router-link to="/Hoja1">Hoja 1</router-link>|
+        <router-link to="/Hoja2">Hoja 2</router-link>
+    </div>
+    <router-view />
+</template>
